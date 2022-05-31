2df9c53ba118a5382978bbce3dd2c013fcc45f69
Fernando Brum <fernandobalcantara@gmail.com>
2022-05-31 00:55:45 -0300

    Adicionar egg com data de criação default

--- src/classes/egg_insert.py
+++ src/classes/egg_insert.py
@@ -1,3 +1,4 @@
+import datetime
 from classes.egg_base import EggBase
 from classes.parser import *
 
@@ -12,6 +13,8 @@ class EggInsert(EggBase):
         self.train_stats = TrainStatParser(value_orig=None)
         self.train_stats_stars = TrainStatStarsParser(value_orig=None, egg_stars=0)
         self.competition_stats = CompetitionParser(value_orig=None)
+
+        self.set_gen(date_created=datetime.datetime.now())
 
     def fill_example_mob(self, name):
         self.name_en = name
