8e2b62e940a987e2912d4b4f35586d94ea69f7a4
Fernando Brum <fernandobalcantara@gmail.com>
2022-05-31 00:58:24 -0300

    Buscar pelo nome EN do egg e criação de atalhos de teclado

--- src/views/window_main.py
+++ src/views/window_main.py
@@ -50,6 +50,9 @@ class MainWindow(BMainWindow):
         self.tab_eggs_layout = QVBoxLayout()
         self.tab_eggs_layout.setAlignment(Qt.AlignTop)
 
+        # shortcut
+        self.__tab_eggs_create_shortcuts()
+
         # buttons
         self.tab_eggs_bt_add = QPushButton('Adicionar')
         self.tab_eggs_bt_add.clicked.connect(self.__tab_eggs_action_add_egg)
@@ -86,6 +89,17 @@ class MainWindow(BMainWindow):
         self.tab_eggs_gb_filter_rb_layout.addSpacing(30)
         self.tab_eggs_gb_filter_rb_layout.addWidget(self.tab_eggs_rb_boss)
         self.tab_eggs_gb_filter_layout.addLayout(self.tab_eggs_gb_filter_rb_layout)
+        #
+        self.tab_eggs_gb_filter_name_en_layout = QHBoxLayout()
+        self.tab_eggs_lbl_name_en_filter = BLabel()
+        self.tab_eggs_lbl_name_en_filter.setText('Nome EN: ')
+        self.tab_eggs_txt_search_name_en_filter = QLineEdit()
+        self.tab_eggs_txt_search_name_en_filter.textChanged.connect(self.__tab_eggs_action_txt_search_name_en_changed)
+        self.tab_eggs_gb_filter_name_en_layout.addWidget(self.tab_eggs_lbl_name_en_filter)
+        self.tab_eggs_gb_filter_name_en_layout.addSpacing(10)
+        self.tab_eggs_gb_filter_name_en_layout.addWidget(self.tab_eggs_txt_search_name_en_filter)
+        self.tab_eggs_gb_filter_layout.addLayout(self.tab_eggs_gb_filter_name_en_layout)
+        #
         self.tab_eggs_gb_filter.setLayout(self.tab_eggs_gb_filter_layout)
 
         # table
@@ -110,6 +124,26 @@ class MainWindow(BMainWindow):
 
         self.__tab_eggs_load_eggs()
 
+    def __tab_eggs_create_shortcuts(self):
+        sc = QShortcut(QKeySequence('Ctrl+N'), self)
+        sc.activated.connect(self.__tab_eggs_action_add_egg)
+
+        sc = QShortcut(QKeySequence('Ctrl+F'), self)
+        sc.activated.connect(self.__tab_eggs_search)
+
+        sc = QShortcut(QKeySequence('Ctrl+Shift+F'), self)
+        sc.activated.connect(self.__tab_eggs_search_clear)
+
+        sc = QShortcut(QKeySequence('Ctrl+L'), self)
+        sc.activated.connect(self.__tab_eggs_action_clear_filter)
+
+        sc = QShortcut(QKeySequence('A'), self)
+        sc.activated.connect(self._tab_eggs_shorcut_all)
+        sc = QShortcut(QKeySequence('M'), self)
+        sc.activated.connect(self._tab_eggs_shorcut_mob)
+        sc = QShortcut(QKeySequence('B'), self)
+        sc.activated.connect(self._tab_eggs_shorcut_boss)
+
     def __tab_eggs_load_eggs(self):
         # print('__tab_eggs_load_eggs()')
         self.tab_eggs_table.b_clear_content()
@@ -130,6 +164,16 @@ class MainWindow(BMainWindow):
                     self.tab_eggs_eggs_list_filter.append(egg)
             else:
                 self.tab_eggs_eggs_list_filter.append(egg)
+
+        aux_list = self.tab_eggs_eggs_list_filter
+        self.tab_eggs_eggs_list_filter = []
+        name_en_txt = self.tab_eggs_txt_search_name_en_filter.text()
+        if name_en_txt != '':
+            for index, egg in enumerate(aux_list):
+                if egg.name_en.lower().find(name_en_txt.lower()) != -1:
+                    self.tab_eggs_eggs_list_filter.append(egg)
+        else:
+            self.tab_eggs_eggs_list_filter = aux_list
 
         for egg in self.tab_eggs_eggs_list_filter:
             self.tab_eggs_table.b_add_row(from_tuple=egg.to_tuple_table())
@@ -157,6 +201,7 @@ class MainWindow(BMainWindow):
 
     def __tab_eggs_action_clear_filter(self):
         self.tab_eggs_rb_all.setChecked(True)
+        self.tab_eggs_txt_search_name_en_filter.setText('')
         self.__tab_eggs_load_eggs()
 
     def __tab_eggs_action_add_egg(self):
@@ -183,3 +228,25 @@ class MainWindow(BMainWindow):
             self.edit_egg_win.window_closed.connect(self.__tab_eggs_close_event)
             self.edit_egg_win.show()
             self.edit_egg_win.b_center_window()
+
+    def __tab_eggs_action_txt_search_name_en_changed(self):
+        self.__tab_eggs_load_eggs()
+
+    def __tab_eggs_search(self):
+        self.tab_eggs_txt_search_name_en_filter.setFocus()
+
+    def __tab_eggs_search_clear(self):
+        self.tab_eggs_txt_search_name_en_filter.setText('')
+        self.tab_eggs_txt_search_name_en_filter.setFocus()
+
+    def _tab_eggs_shorcut_all(self):
+        self.tab_eggs_rb_all.setChecked(True)
+        self.__tab_eggs_action_rb_all()
+
+    def _tab_eggs_shorcut_mob(self):
+        self.tab_eggs_rb_mob.setChecked(True)
+        self.__tab_eggs_action_rb_mob()
+
+    def _tab_eggs_shorcut_boss(self):
+        self.tab_eggs_rb_boss.setChecked(True)
+        self.__tab_eggs_action_rb_boss()
